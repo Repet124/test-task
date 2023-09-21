3dc27a2dd04d51db07bd3e5621dd6e8dc998378b
repet124 <hlop4ik124@gmail.com>
2023-09-21 16:10:24 +0300

    Login tests

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+use App\Models\User;
+
 class LoginTest extends TestCase
 {
 	use RefreshDatabase;
@@ -19,5 +21,49 @@ class LoginTest extends TestCase
 	{
 		$response = $this->get('/login');
 		$response->assertStatus(200);
+	}
+
+	public function test_success_authorization() {
+		$password = 'test_password';
+		$user = $this->createUser($password);
+
+		$response = $this->post('/login', [
+			'login' => $user->login,
+			'password' => $password
+		]);
+
+		$response->assertStatus(200);
+		$response->assertRedirect('/');
+		$this->assertAuthenticatedAs($user);
+	}
+
+	public function test_fail_authorization() {
+		$password = 'test_password';
+		$user = $this->createUser($password);
+
+		$response = $this->post('/login', [
+			'login' => 'abracadabra',
+			'password' => $password
+		]);
+
+		$response->assertStatus(403);
+		$this->assertGuest();
+
+		$response = $this->post('/login', [
+			'login' => $user->login,
+			'password' => 'sadfasdf'
+		]);
+
+		$response->assertStatus(403);
+		$this->assertGuest();
+	}
+
+	protected function createUser($password): User {
+		return User::create([
+			'login' => 'test_login',
+			'first_name' => 'test_first_name',
+			'last_name' => 'test_last_name',
+			'password' => $password
+		]);
 	}
 }
