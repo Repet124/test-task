723301d4399c7015d7f333d4961feffe6366714f
repet124 <hlop4ik124@gmail.com>
2023-09-22 19:38:52 +0300

    tests list for EventsAPI

---
+++ tests/Feature/EventAPITest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EventAPITest extends TestCase {
+
+	public function test_getting_events_list() {
+		// code...
+	}
+
+	public function test_success_creating_event() {
+		// code...
+	}
+
+	public function test_fail_creating_event() {
+		// code...
+	}
+
+	public function test_destroy_event() {
+		// code...
+	}
+
+	public function test_request_for_involving_user_in_event() {
+		// code...
+	}
+
+	public function test_to_dismiss_user_from_event() {
+		// code...
+	}
+
+}
