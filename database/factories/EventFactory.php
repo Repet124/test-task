8673862eebc1612ffc09e060972d273541b98023
repet124 <hlop4ik124@gmail.com>
2023-09-22 23:49:46 +0300

    EventFactory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array
+	{
+		return [
+			'title' => fake()->unique()->sentence(3),
+			'description' => fake()->sentence(10),
+			'creator_id' => User::factory()->create()->id
+		];
+	}
+}
