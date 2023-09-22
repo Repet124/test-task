8bbec5af7097475d95aa313f5e63dc95dfcda6f7
repet124 <hlop4ik124@gmail.com>
2023-09-22 22:46:58 +0300

    test_getting_events_list

--- tests/Feature/EventAPITest.php
+++ tests/Feature/EventAPITest.php
@@ -2,18 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\Models\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class EventAPITest extends TestCase {
 
+	use RefreshDatabase;
+
 	public function test_getting_events_list() {
-		// code...
+
+		$event = Event::factory()->create()[0];
+
+		$response = $this->get('/api/events');
+
+		$response
+			->assertStatus(201)
+			->assertJson(fn (AsserttableJson $json) =>
+				$json->has('events', 1,fn (AsserttableJson $json) =>
+					$json->where('title', $event->title)
+						->where('description', $event->descripton)
+						->where('creator.id', $event->creator->id)
+						->where('creator.first_name', $event->creator->first_name)
+						->where('creator.last_name', $event->creator->last_name)
+						->has('members', 3, fn (AsserttableJson $json) =>
+							$json->where('id', $event->members[0]->id)
+								->etc()
+						)
+				)
+			);
+
 	}
 
 	public function test_success_creating_event() {
-		// code...
+
 	}
 
 	public function test_fail_creating_event() {
