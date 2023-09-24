2201f4e6742c99c829475868ea0098a2b6afda1f
repet124 <hlop4ik124@gmail.com>
2023-09-24 21:08:59 +0300

    fix bugs in registration

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -2,10 +2,16 @@ import './bootstrap';
 
 let password = document.getElementById('inputPassword1'),
 	passwordConfirm = document.getElementById('inputConfirmPassword1'),
+	confirmInfo = document.getElementById('confirmInfoJS'),
 	btn = document.getElementById('btnSubmitJS');
 
 function confirm() {
 	btn.disabled = (!password.value) || (password.value !== passwordConfirm.value);
+	if (btn.disabled && passwordConfirm.value) {
+		confirmInfo.classList.remove('d-none')
+	}else {
+		confirmInfo.classList.add('d-none')
+	}
 }
 
 if (passwordConfirm) {
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,12 +10,21 @@
 			<input required name="login" type="text" class="form-control" id="inputLogin1" value={{old('login')}}>
 		</div>
 		<div class="mb-3">
+			<label for="inputFirstName1" class="form-label">Имя</label>
+			<input required name="first_name" type="text" class="form-control" id="inputFirstName1" value={{old('first_name')}}>
+		</div>
+		<div class="mb-3">
+			<label for="inputLastName1" class="form-label">Фамилия</label>
+			<input required name="last_name" type="text" class="form-control" id="inputLastName1" value={{old('last_name')}}>
+		</div>
+		<div class="mb-3">
 			<label for="inputPassword1" class="form-label">Пароль</label>
 			<input required name="password" type="password" class="form-control" id="inputPassword1">
 		</div>
 		<div class="mb-3">
 			<label for="inputConfirmPassword1" class="form-label">Подтверждение пароля</label>
 			<input required name="confirm_password" type="password" class="form-control" id="inputConfirmPassword1">
+			<p id="confirmInfoJS" class="d-none mt-3 text-danger">Пароли должны совпадать</p>
 		</div>
 		<button id="btnSubmitJS" type="submit" class="btn btn-primary">Регистрация</button>
 	</form>
