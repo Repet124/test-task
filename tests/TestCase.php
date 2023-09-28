0ab02bf35bac34b0117064686ee64f48e179b830
repet124 <hlop4ik124@gmail.com>
2023-09-28 16:00:05 +0300

    signatures for common tests cases 	renamed:    tests/Feature/EventAPITest.php -> tests/Feature/APITest.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+	use CreatesApplication;
+
+	protected function notAuth($response){
+
+	}
+
+	protected function notFoundWithMessage($response, $message='Запрашиваемый ресурс не найден') {
+		// code...
+	}
+
+	protected function errWithMessage($response, $message) {
+		// code...
+	}
 }
