963d28a3395222d1529e1352a416e4bb5f830223
repet124 <hlop4ik124@gmail.com>
2023-09-28 16:04:48 +0300

    common tests complited

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,14 +9,23 @@ abstract class TestCase extends BaseTestCase
 	use CreatesApplication;
 
 	protected function notAuth($response){
-
+		$response->assertStatus(403);
+		$response->assertJson([
+			'error' => 'Пользователь не авторизован'
+		]);
 	}
 
 	protected function notFoundWithMessage($response, $message='Запрашиваемый ресурс не найден') {
-		// code...
+		$response->assertStatus(404);
+		$response->assertJson([
+			'error' => $message
+		]);
 	}
 
 	protected function errWithMessage($response, $message) {
-		// code...
+		$response->assertOk();
+		$response->assertJson([
+			'error' => $message
+		]);
 	}
 }
