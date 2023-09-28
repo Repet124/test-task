0acb3442e2c716ac03e177dfd0d6e5d4b7ec9c83
repet124 <hlop4ik124@gmail.com>
2023-09-28 21:58:29 +0300

    Test Refactoring started Error after run migrate with seed command

---
+++ database/seeders/TestingSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestingSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+
+		// test user
+		$testUser = User::factory()
+			->testUser()
+			->hasEvents(2)
+			->create();
+
+		// test user involvments into self last event
+		$testUser->events->last()->members()->attach($testUser);
+
+		// creating users with events (random count)
+		User::factory()
+			->count(10)
+			->addRandomCountEvents(from: 0, to: 3)
+			->create();
+
+		//test user involvments into other user event
+		Event::whereNot('creator_id', $testUser->id)->first()->members()->attach($testUser);
+
+	}
+}
